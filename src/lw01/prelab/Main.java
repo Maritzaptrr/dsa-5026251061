62760b6d1d403d387b62d9cb812daefce2993506
Maritza Putri Evita <Maritzaaputri@gmail.com>
2026-09-20 18:52:39 +0700

    Labwork 01 - Prelab

---
+++ src/lw01/prelab/Main.java
@@ -0,0 +1,33 @@
+package lw01.prelab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<PrintJob> jobs = new ArrayList<>();
+
+        Scanner scanner = new Scanner(Main.class.getResourceAsStream("jobs.txt"));
+
+        while (scanner.hasNext()) {
+            String type = scanner.next();
+            String id = scanner.next();
+            int pages = scanner.nextInt();
+
+            if (type.equals("MONO")) {
+                jobs.add(new MonoPrint(id, pages));
+            } else if (type.equals("COLOUR")) {
+                jobs.add(new ColourPrint(id, pages));
+            }
+        }
+
+        scanner.close();
+
+        for (PrintJob job : jobs) {
+            System.out.println(job.summary());
+        }
+    }
+}
